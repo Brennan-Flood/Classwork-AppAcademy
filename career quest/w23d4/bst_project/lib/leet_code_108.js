25d562408fcb736c554681344648926ad8e1260b
Brennan <flood.brennan@gmail.com>
2020-01-16 18:19:59 -0500

    add w23d4 cw

--- career quest/w23d4/bst_project/lib/leet_code_108.js
+++ career quest/w23d4/bst_project/lib/leet_code_108.js
@@ -3,5 +3,12 @@
 
 
 function sortedArrayToBST(nums) {
+  if (!nums.length) return null;
 
+  let midIdx = Math.floor(nums.length / 2);
+  let root = new TreeNode(nums[midIdx]);
+  root.left = sortedArrayToBST(nums.slice(0, midIdx));
+  root.right = sortedArrayToBST(nums.slice(midIdx + 1));
+
+  return root;
 }
