766e46becc82ba57bc4aa8cb17cb9e46c1744199
Brennan <flood.brennan@gmail.com>
2020-01-16 18:20:15 -0500

    add w23d4 cw

--- career quest/w23d4/bst_project/lib/leet_code_110.js
+++ career quest/w23d4/bst_project/lib/leet_code_110.js
@@ -2,6 +2,13 @@
 //  https://leetcode.com/problems/balanced-binary-tree/
 
 
-function isBalanced(root) {
+function getHeight(root) {
+  if (!root) return -1;
+  return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+}
 
+function isBalanced(root) {
+  if (!root) return true;
+  let heightDifference = Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1;
+  return heightDifference && isBalanced(root.left) && isBalanced(root.right);
 }
