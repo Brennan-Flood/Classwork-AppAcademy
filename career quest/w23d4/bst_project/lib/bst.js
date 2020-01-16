c6f39d5673763e2c29d268d069e8accf72a5ccdc
Brennan <flood.brennan@gmail.com>
2020-01-16 18:18:55 -0500

    add w23d4 cw

--- career quest/w23d4/bst_project/lib/bst.js
+++ career quest/w23d4/bst_project/lib/bst.js
@@ -8,7 +8,58 @@ class TreeNode {
 
 
 class BST {
-   
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val, root = this.root) {
+        if (!this.root) {
+            this.root = new TreeNode(val);
+            return;
+        }
+
+        if (val < root.val) {
+            if (!root.left) {
+                root.left = new TreeNode(val);
+            } else {
+                this.insert(val, root.left);
+            }
+        } else {
+            if (!root.right) {
+                root.right = new TreeNode(val);
+            } else {
+                this.insert(val, root.right);
+            }
+        }
+    }
+
+    searchRecur(val, root = this.root) {
+        if (!root) return false;
+
+        if (val < root.val) {
+            return this.searchRecur(val, root.left);
+        } else if (val > root.val) {
+            return this.searchRecur(val, root.right);
+        } else {
+            return true;
+        }
+    }
+
+    searchIter(val) {
+        let curr = this.root;
+
+        while (curr) {
+            if (val < curr.val) {
+                curr = curr.left;
+            } else if (val > curr.val) {
+                curr = curr.right;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
 
 module.exports = {
